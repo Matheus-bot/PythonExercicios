93662614fa01a534226b05880fc76a935bc024ba
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2022-12-03 10:26:54 -0300

    Mais 2 exercícios

---
+++ ex064.py
@@ -0,0 +1,12 @@
+# 64 - Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o
+# valor 999, que é a condição de parada. No final mostre quantos números foram digitados e qual foi a soma entre eles
+# (desconsiderando o flag(999)).
+
+núm = cont = soma = 0
+while núm != 999:
+    núm = int(input('Digite um número [999 para parar]:'))
+    if núm != 999:
+        soma += núm
+        cont += 1
+print('Você digitou {} números e a soma entre eles foi {}.'.format(cont, soma))
+
---
+++ ex065.py
@@ -0,0 +1,26 @@
+# 65 - Crie um programa que leia vários números inteiros pelo teclado. No final da execução,
+# mostre a média entre todos os valores e qual foi o maior e o menor valores lidos.
+# O programa deve perguntar se ele quer ou não continuar a digitar valores
+
+
+resp = 'S'
+soma = quant = média = maior = menor = 0
+while  resp in 'Ss':
+    núm = int(input('Digite um número:'))
+    soma += núm
+    quant += 1
+    if quant == 1:
+        maior = menor = núm
+    else :
+        if núm > maior:
+            maior = núm
+        if núm < menor:
+            menor = núm
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+média = soma / quant
+print('Você digitou {} números e a média foi {}'. format(quant, média))
+print('O maior valor foi {} e o menor foi {}'.format(maior, menor))
+
+
+
+
