03dc4b62192311b574d467872c3fb9dbb58e681f
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-01-17 09:50:12 -0300

    update

---
+++ EstruturaSequencial.py
@@ -0,0 +1,31 @@
+# Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+'''print('Hello World')'''
+# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+'''num = int(input("Informe um número:"))
+print(f'O número informado foi: {num}')
+'''''
+# Faça um Programa que peça dois números e imprima a soma.
+'''n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+print(f'A soma é {n1 + n2}')
+'''
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+'''
+nota1 = float(input('Qual a primeira nota? '))
+nota2 = float(input('Qual a segunda nota? '))
+nota3 = float(input('Qual a terceira nota? '))
+nota4 = float(input('Qual a quarta nota? '))
+media = (nota1 + nota2 + nota3 + nota4)/ 4
+print(f'A média é {media}')
+'''
+# Faça um Programa que converta metros para centímetros.
+'''
+metros = float(input('Quantos metros ? '))
+print(f'{metros} metros equivalem a  {metros * 100} centimetros')
+'''''
+
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio = float(input('Qual valor do raio? '))
+r = raio * raio
+print(f'O valor da área é {r * 3.14}')
