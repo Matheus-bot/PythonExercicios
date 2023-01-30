7cac7507554bd324d92ee7e5732b103c92561b8e
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-01-30 15:07:24 -0300

    Update ex036.py

--- ex036.py
+++ ex036.py
@@ -2,25 +2,20 @@
 # o valor da casa, o salário do comprador e em quantos anos ele vai pagar
 # Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.
 
-'''valor = float(input('Qual o valor da casa ? '))
-salario = float(input('Qual seu salário? '))
-qntanos = int(input('Em quantos anos você ira pagar?'))
-
-porcentagem = salario * 30/100
-pm = valor * qntanos
-if pm > porcentagem:
-    print('O emprestimo foi negado ')
-else:
-    print('Parabens seu emprestimo foi aprovado!!!')'''
-
+from time import sleep
+print('='*25)
+print('\033[0;34m BEM VINDO AO BANCO MATT\033[m')
+print('='*25)
 casa = float(input('Valor da casa:R$'))
 salário = float(input('Saláriodo coomprador: R$'))
 anos = int(input('Quantos anos do financiamento? '))
 prestação = casa / (anos * 12)
 mínimo = salário * 30 / 100
-print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, anos), end ='')
-print('A prestação será de R${:.2f}'.format(prestação))
+print('ANALISANDO...')
+sleep(3)
+print(f'Para pagar uma casa de R${casa:.2f} em {anos} anos ', end='')
+print(f'A prestação será de R${prestação:.2f}')
 if prestação <= mínimo:
-    print('Empréstimo pode ser CONCEDIDO!')
+    print('\033[0;34mEmpréstimo pode ser CONCEDIDO!\033[m')
 else:
-    print('Emprestimo NEGADO!')
+    print('\033[0;31mEmprestimo NEGADO!\033[m')
