99d0ee953fbd3678a0fad654329bee61d19898cd
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-02-11 10:16:33 -0300

    Update ex058.py

--- ex058.py
+++ ex058.py
@@ -19,3 +19,24 @@ while not acertou:
         elif jogador > computador:
             print('Menos... Tente mais uma vez')
 print('Acertou com {} tentativas. Parabéns '.format(palpites))
+
+'''
+
+from random import randint
+print('Pensei em um número entre 0 e 5')
+jogador = ''
+computador = randint(0,5)
+tentativas = 0
+while computador != jogador:
+    jogador = int(input('Em que numero eu pensei ?'))
+    tentativas += 1
+    if jogador == computador:
+        print('Parabens você acertou')
+    else:
+        if jogador > computador:
+            print('menos tente mais uma vez ')
+        elif jogador < computador:
+            print('Mais...')
+
+print(f'Foram {tentativas} tentativas')'''
+
