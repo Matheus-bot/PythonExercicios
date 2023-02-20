1bf979bd5e82c93e5fd529dafb669ebc183a236b
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-02-20 12:08:43 -0300

    Update ex063.py

--- ex063.py
+++ ex063.py
@@ -9,11 +9,11 @@ n = int(input('Quantos termos você quer mostrar? '))
 t1 = 0
 t2 = 1
 print('~'*30)
-print('{} -> {}'.format(t1, t2), end='')
+print(f'{t1} -> {t2}', end='')
 cont = 3
 while cont <= n:
     t3 = t1 + t2
-    print(' -> {}'.format(t3), end='')
+    print(f' -> {t3}', end='')
     t1 = t2
     t2 = t3
     cont += 1
