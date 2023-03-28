7357d78893c7960d1adc0be274e6a41612edc753
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-03-28 10:10:02 -0300

    updated

--- ex080.py
+++ ex080.py
@@ -3,7 +3,7 @@
 
 lista = []
 c = 0
-for c in range(0,5):
+for c in range(0, 5):
     n = int(input('Digite um valor: '))
     if c == 0 or n > lista[-1]:
         lista.append(n)
---
+++ ex081.py
@@ -0,0 +1,25 @@
+# 81 - Crie um programa que vai ler vários números e colocar em uma lista. depois disso, mostre:
+# a) Quantos números foram digitados.
+# b) A lista de valores, ordenada de forma decrescente.
+# c) Se o valor 5 foi digitado e está ou não na lista
+
+'''lista = []
+continuar = 'S'
+while continuar == 'S':
+    n = lista.append(int(input('Digite um valor: ')))
+
+    continuar = str(input('Quer continuar? ')).upper()
+
+    if continuar == 'N':
+        break
+
+print(f'Foram digitados {len(lista)} valores')
+lista.sort(reverse=True)
+print(f'A lista em ordem decrescente: {lista} ')
+if 5 in lista:
+    print('Achei o número 5 na lista')
+else:
+    print('Não achei o número 5 na lista')'''
+
+
+
