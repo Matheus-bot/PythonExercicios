9233fd5fde32ee9fefedda6d641ddb3a7e55470d
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-05-06 10:29:51 -0300

    update

---
+++ ex085.py
@@ -0,0 +1,16 @@
+# 85 - Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que
+# mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
+
+
+núm = [[], []]
+valor = 0
+for c in range(1,8):
+    valor = int(input(f'Digite o {c}º valor: '))
+    if valor % 2 == 0:
+        núm[0].append(valor)
+    else:
+        núm[1].append(valor)
+
+print('-=' * 30)
+print(f'Os valores pares digitados foram: {sorted(núm[0])}')
+print(f'Os valores impares digitados foram: {sorted(núm[1])} ')
