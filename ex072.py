742f7f108a41f35cc7cd1b44b05d63dd6ac1061f
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-05-16 09:39:24 -0300

    Update ex072.py

--- ex072.py
+++ ex072.py
@@ -2,28 +2,32 @@
 # com uma contagem por extenso, de zero até vinte.
 # Seu programa deverá ler um número pelo teclado(entre 0 e 20)
 # e mostra-lo por extenso.
+from time import sleep
+cont = ('Zero', 'um', 'dois', 'três', 'quatro', 'cinco',
+        'seis', 'sete', 'oito', 'nove', 'dez',
+        'onze', 'doze', 'Treze', 'catorze',
+        'quinze', 'dezesseis', 'dezessete', 'dezoito',
+        'dezenove', 'vinte'
+        )
+print('='*30)
+print('=== NÚMEROS POR EXTENSO ===')
+print('='*30)
+resp = 'S'
 
+while True:
+    while resp in 'Ss':
+        num = int(input('Digite um número entre 0 e 20: '))
 
-numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco',
-           'seis', 'sete', 'oito', 'nove', 'dez', 'onze',
-           'doze', 'treze', 'catorze', 'quinze', 'dezesseis',
-           'dezessete', 'dezoito', 'dezenove', 'vinte')
-
-continuar = 'S'
-while continuar in 'SN':
-    while continuar == 'S':
-        continuar = str(input('Quer continuar? [S/N]')).strip().upper()[0]
-        num = int(input('Digite um número entre 0 e 20'))
-        print(numeros[num])
         if 0 <= num <= 20:
             break
+        print('Tente novamente')
 
-        if continuar == 'N':
-            break
-
-print('Acabou')
-
-
+    print(f'Você digitou o numero {cont[num]}')
+    resp = str(input('Quer continuar ? [S/N]'))
+    if resp in 'Nn':
+        sleep(1)
+        print('========= FINALIZANDO =========')
+        break
 
 
 
