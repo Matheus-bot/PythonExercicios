538c232f8902e8bb35f8942a567b62ad7ecb8753
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-06-21 10:31:41 -0300

    update

--- ex090.py
+++ ex090.py
@@ -1,6 +1,6 @@
 # 90- Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
 # No final mostre o conteúdo da estrutura na tela
-
+"""
 aluno = dict()
 aluno['nome'] = str(input('Nome: '))
 aluno['media'] = float(input(f'Media de {aluno["nome"]}: '))
@@ -13,4 +13,46 @@ else:
 print('-=' * 30)
 for k, v in aluno.items():
     print(f' - {k} é igual a {v}')
+
+"""
+
+dados = dict()
+
+dados['nome'] = str(input('Nome: '))
+dados['media'] = float(input(f'Média de {dados["nome"]} '))
+
+print('-='* 30)
+aprovado = False
+if dados['media'] > 6:
+    dados['situação'] = 'Aprovado'
+
+else:
+    dados['situação'] = 'Reprovado'
+
+for k, v in dados.items():
+    print(f'{k} é igual a {v}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ ex094.py
@@ -0,0 +1,6 @@
+# 94 - Crie um programa que leia nome, sexo e idade de varías pessoas, guardando os dados de cada pessoa em um
+# dicionário em uma lista. No final, mostre:
+# a) Quantas pessoas foram cadastradas.
+# b) A média de idade do grupo.
+# c) Uma lista com todas  as mulheres.
+# d) Um lista com todas a s pessoas com idade acima da média.
