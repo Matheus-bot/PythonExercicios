d596d6867d9ae123929707fbc719cce8c62cf823
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-06-22 10:16:58 -0300

    update

--- ex091.py
+++ ex091.py
@@ -10,12 +10,31 @@ jogo = {'jogador1': randint(1,6),
         'jogador4': randint(1,6)
         }
 ranking = list()
+
 for k, v in jogo.items():
     print(f'   {k} tirou {v} no dado ')
     sleep(1)
 ranking = sorted(jogo.items(), key=itemgetter(1), reverse= True)
-print('=-'* 30)
 print('  == RANKING DOS JOGADORES ==')
 for i, v in enumerate(ranking):
     print(f'   {i+1}° lugar: {v[0]} com {v[1]}')
     sleep(1)
+print('=-'* 30)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
