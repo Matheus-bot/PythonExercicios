12271003afb31fcac192e733972e5bd5986b3a9e
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-07-16 22:08:14 -0300

    update

---
+++ ex107/teste.py
@@ -0,0 +1,6 @@
+import moeda
+
+p = float(input(f'Digite o preço: '))
+print(f'A metade de {p} é {moeda.metade(p)}')
+print(f'O dobro de {p} é {moeda.dobro(p)}')
+print(f'Aumentando 10%, temos {moeda.aumentar(p, 10)}')
