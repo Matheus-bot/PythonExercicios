02891e84ceef99795adfbee004b666c8fbedba9f
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-07-16 22:36:30 -0300

    update

---
+++ ex110/moeda.py
@@ -0,0 +1,47 @@
+def aumentar(preço=0, taxa=0, formato=False):
+    """
+    -> Função para calcular o aumento de um preço
+    :param preço: valor do preço a ser ajustado
+    :param taxa: valor a ser aumentado
+    :param formato: paramêtro opcional(se quer a formatação da moeda ou não)
+    :return: retorna o aumento
+    """
+    res = preço + (preço * taxa / 100)
+    return res if formato is False else moeda(res)
+
+
+def diminuir(preço=0, taxa=0, formato=False):
+    res = preço - (preço * taxa / 100)
+    return res if formato is False else moeda(res)
+
+
+def dobro(preço=0, formato=False):
+    res = preço * 2
+    return res if not formato else moeda(res)
+
+
+def metade(preço=0, formato=False):
+    res = preço / 2
+    return res if not formato else moeda(res)
+
+
+def moeda(preço=0, moeda='R$'):
+    return f'{moeda}{preço:>.2f}'.replace('.', ',')
+
+
+def resumo(preço=0, taxaa=10, taxar=5):
+    print('-' * 30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-' * 30)
+    print(f'Preço analisado: \t{moeda(preço)}')
+    print(f'Dobro do preço: \t{dobro(preço, True)}')
+    print(f'Metade do preço: \t{metade(preço, True)}')
+    print(f'{taxaa}% de aumento: \t{aumentar(preço, taxaa, True)}')
+    print(f'{taxar}% de redução: \t\t{diminuir(preço, taxar, True)}')
+    print('-' * 30)
+
+
+
+
+
+
